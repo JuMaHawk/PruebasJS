5382c5156f729a9a5f81450093dcbeb3fdd80519
JuMaHawk <jmferrero@gmail.com>
2023-11-02 18:11:02 -0300

    Resolvi el problema con Agregar productos

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-productos = [];
+const listadoProductos = [];
 
-function mostrarProductos (producto){
-  productos.forEach(element => {
-        
-  });
-}
+
 //Traigo del html el boton para agregar productos.
 let botonAgregar = document.getElementById("botonAgregar")
 
@@ -14,25 +10,39 @@ botonAgregar.addEventListener("click", () => {
   botones.remove()
   let inputDatos = document.createElement("div");
   let saludo = document.getElementById("saludo")
-  saludo.innerText = "Ingrese los datos del producto que desea agregar" 
+  saludo.innerText = "Ingrese los datos del producto que desea agregar"
   inputDatos.innerHTML = `
-<form id="formulario">
+    <form id="formulario">
     <input type="text" placeholder="Categoria">
     <input type="text" placeholder="Nombre">
     <input type="number" placeholder="Precio">
     <input type="submit" value="Enviar datos">
-</form>
-`
-document.body.append(inputDatos);
+</form>`
+  document.body.append(inputDatos);
+
+  let formulario = document.getElementById("formulario");
+
+  formulario.addEventListener("submit", (e) => {
+    let inputs = e.target.children;
+    let categoria = inputs[0].value;
+    let nombre = inputs[1].value
+    let precio = inputs[2].value;
+    listadoProductos.push({ categoria, nombre, precio });
+  })
 });
 
-formulario.addEventListener(submit, (e)=> {
-  e.preventDefault(); 
-  let inputs = e.target.children
-  let categoria = inputs[0].value;
-  let nombre = inputs[1].value
-  let precio = inputs[2].value;
-
-  productos.push({categoria, nombre, precio})
-console.log(productos)
+let mostrarStock = document.getElementById("botonMostrar");
+mostrarStock.addEventListener("click", () => {
+  listadoProductos.forEach((item) => {
+    alert("funciona")
+    let nuevoDiv = document.createElement("div");
+    div.innerHTML = `
+    <h2>Categoria: ${item.categoria}</h2>
+    <p>Producto: ${item.nombre}</p>
+    <p>Precio: ${item.precio}</p>
+    `
+    alert(listadoProductos)
+    document.body.append(nuevoDiv)
+  });
 })
+
