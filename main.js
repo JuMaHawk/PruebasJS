a75f7d8ce468297cf97482238b95a2af74d59635
JuMaHawk <jmferrero@gmail.com>
2023-11-06 00:59:39 -0300

    Botones ok

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 listadoProductos = [];
-const listadoLS = JSON.parse(localStorage.getItem("listadoProductos"))
+const listadoLS = JSON.parse(localStorage.getItem("listOfProduct"))
 console.log(listadoLS)
 if(listadoLS){
     listadoProductos = listadoLS;
 }
 
 
-
 const guardarLS = (clave , valor) => localStorage.setItem(clave,valor);
 
-//Traigo del html el boton para agregar productos.
+//-----------------------AGREGAR PRODUCTOS--------------------------
 let botonAgregar = document.getElementById("botonAgregar")
 
 //Le aplico un evento cuando le hacen click. Creando un formulario para ingresar los datos del producto.
@@ -37,29 +36,35 @@ botonAgregar.addEventListener("click", () => {
     let nombre = inputs[1].value;
     let precio = inputs[2].value;
     nuevoProducto = {categoria, nombre, precio};
-    console.log(nuevoProducto);
-    // const newProd = JSON.stringify(nuevoProducto);
-    // localStorage.setItem("PUTO", newProd)
-
     listadoProductos.push(nuevoProducto)
-    const listProd = JSON.stringify(listadoProductos)  
-    guardarLS("listadoProdcutos", JSON.stringify(listadoProductos))
+    // const listProd = JSON.stringify(listadoProductos)  
+    guardarLS("listOfProduct", JSON.stringify(listadoProductos))
   })
 });
 
-
+//----------------------------MOSTRAR PRODUCTOS-----------------------------
 let mostrarStock = document.getElementById("botonMostrar");
 mostrarStock.addEventListener("click", () => {
-    console.log(listadoProductos);
+  let nuevoDiv = document.createElement("div");
+  nuevoDiv.id = "muestreo"
   listadoProductos.forEach((item) => {
-    alert("funciona")
-    let nuevoDiv = document.createElement("div");
-    div.innerHTML = `
+    listadoProductos = [];
+    nuevoDiv.innerHTML = `
     <h2>Categoria: ${item.categoria}</h2>
     <p>Producto: ${item.nombre}</p>
     <p>Precio: ${item.precio}</p>
     `
-    alert(listadoProductos)
     document.body.append(nuevoDiv)
   });
+})
+//TENGGO QUE AGREGAR UN MENSAJE VACIO DONDE SE VAYAN SUMANDO LOS PRODUCTOS EN CADA ITERACION DEL FOREACH DE LISTADO DE PRODUCTOS          
+
+//-----------------------------ELIMINAR PRODUCTOS----------------------------
+let botonEliminar = document.getElementById("botonEliminar");
+botonEliminar.addEventListener("click", () =>{
+  localStorage.clear();
+  listadoProductos = [];
+  let nuevoDiv = document.getElementById("muestreo")
+  nuevoDiv.remove()
+  console.log(listadoProductos)
 })
